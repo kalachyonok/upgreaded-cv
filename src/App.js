d739e150775c69872006c4126e8c203357c647cb
Olga Kalacheva <kalach2256954@gmail.com>
2023-12-26 17:32:59 +0400

    create vector component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import style from "./App.module.css";
 import { Header } from "./Components/Header";
+import { Vector } from "./Components/Vector";
 
 function App() {
   return (
     <div className={style.page}>
       <Header />
+      <Vector />
     </div>
   );
 }
---
+++ src/Components/Vector.js
@@ -0,0 +1,9 @@
+import style from "./Vector.module.css";
+
+export const Vector = () => {
+  return (
+    <div className={style.container}>
+      <div className={style.vector}></div>
+    </div>
+  );
+};
